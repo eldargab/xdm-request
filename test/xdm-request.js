c0e6df5e0e3918c71e2db1e995ed6dc6ba3c7141
Eldar Gabdullin <eldargab@gmail.com>
2012-10-21 21:18:33 +0400

    more tests

--- test/xdm-request.js
+++ test/xdm-request.js
@@ -12,6 +12,15 @@ describe('Xdm Request', function () {
     })
   })
 
+  it('GET json', function (done) {
+    request.get('/json', function (res) {
+      assert(res.body[0] == 'foo')
+      assert(res.body[1] == 'bar')
+      assert(res.type == 'application/json')
+      done()
+    })
+  })
+
   it('simple POST', function (done) {
     request.post('/echo', function (res) {
       assert(res.body.method == 'POST')
@@ -19,12 +28,40 @@ describe('Xdm Request', function () {
     })
   })
 
-  it('GET json', function (done) {
-    request.get('/json', function (res) {
-      assert(res.body[0] == 'foo')
-      assert(res.body[1] == 'bar')
-      assert(res.type == 'application/json')
-      done()
+  describe('.send()', function () {
+    it('Should send json by default', function (done) {
+      request
+        .post('/echo')
+        .send({foo: 'bar'})
+        .end(function (res) {
+          assert(res.body.header['content-type'] == 'application/json')
+          assert(res.body.body.foo == 'bar')
+          done()
+        })
+    })
+
+    it('Should send x-www-form-urlencoded for corresponding Content-Type', function (done) {
+      request
+        .post('/echo')
+        .send({foo: 'bar'})
+        .type('application/x-www-form-urlencoded')
+        .end(function (res) {
+          assert(res.body.header['content-type'] == 'application/x-www-form-urlencoded')
+          assert(res.body.body.foo == 'bar')
+          done()
+        })
+    })
+
+    it('Should merge', function (done) {
+      request
+        .post('/echo')
+        .send({foo: 'bar'})
+        .send({bar: 'baz'})
+        .end(function (res) {
+          assert(res.body.body.foo == 'bar')
+          assert(res.body.body.bar == 'baz')
+          done()
+        })
     })
   })
 
